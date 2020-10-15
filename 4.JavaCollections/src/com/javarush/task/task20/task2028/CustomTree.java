74c914f8f2a355d96d37c121a763a9e951838417
swd762 <ilyakazalin@yandex.ru>
2020-10-15 12:10:40 +0400

    fully completed task 2028

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,37 +1,161 @@
 package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /* 
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
-
     Entry<String> root;
-    private List<Entry<String>> treeList = new ArrayList<>();
+    private int size = 0;
+
+    public CustomTree() {
+        this.root = new Entry<>("0");
+    }
+
+    @Override
+    public boolean add(String s) {
+        Queue<Entry<String>> queue = new ArrayDeque<>();
+        Entry<String> entry = root;
+        Entry<String> child = new Entry<>(s);
+        queue.add(entry);
+
+        checkTree();
+
+        while (!queue.isEmpty()) {
+            if (entry.isAvailableToAddChildren()) {
+                child.parent = entry;
+
+                if (entry.availableToAddLeftChildren)
+                    entry.leftChild = child;
+                else
+                    entry.rightChild = child;
+
+                entry.checkChildren();
+                size++;
+                return true;
+            }
+            if (entry.leftChild != null)
+                queue.add(entry.leftChild);
+            if (entry.rightChild != null)
+                queue.add(entry.rightChild);
+            if (!queue.isEmpty())
+                entry = queue.poll();
+        }
+        return false;
+    }
 
 
     @Override
     public int size() {
-        return treeList.size()-1;
+        return this.size;
     }
 
     public String getParent(String s) {
-        for (Entry<String> entry: treeList) {
-            if (entry.elementName.equals(s)) {
-                return entry.parent.elementName;
-            }
+        try {
+            return findEntry(s).parent.elementName;
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    private Entry<String> findEntry(String s) {
+        Queue<Entry<String>> queue = new ArrayDeque<>();
+        Entry<String> entry = root;
+        queue.add(entry);
+
+        while (!queue.isEmpty()) {
+            if (entry.elementName.equals(s))
+                return entry;
+            if (entry.leftChild != null)
+                queue.add(entry.leftChild);
+            if (entry.rightChild != null)
+                queue.add(entry.rightChild);
+            if (!queue.isEmpty())
+                entry = queue.poll();
         }
         return null;
     }
 
-    @Override
-    public String set(int index, String element) {
-        throw new UnsupportedOperationException();
+    public boolean remove(Object o) {
+        if (!(o instanceof String)) throw new UnsupportedOperationException();
+
+        Entry<String> entry;
+        Entry<String> parent;
+
+        try {
+            entry = findEntry((String) o);
+            parent = entry.parent;
+            size -= calculateChilds(entry);
+            if (parent.leftChild == entry)
+                parent.leftChild = null;
+            if (parent.rightChild == entry)
+                parent.rightChild = null;
+
+            return true;
+        } catch (NullPointerException e) {
+            return false;
+
+        }
+
+    }
+
+    private int calculateChilds(Entry<String> entry) {
+
+        Queue<Entry<String>> queue = new ArrayDeque<>();
+        int counter = 1;
+
+        do {
+            if (entry.leftChild != null) {
+                queue.add(entry.leftChild);
+                counter++;
+            }
+            if (entry.rightChild != null) {
+                queue.add(entry.rightChild);
+                counter++;
+            }
+            if (!queue.isEmpty())
+                entry = queue.poll();
+        } while (!queue.isEmpty());
+
+        return counter;
+    }
+
+    private void checkTree() {
+        Queue<Entry<String>> queue = new ArrayDeque<>();
+        Entry<String> entry = root;
+        queue.add(entry);
+
+        while (!queue.isEmpty()) {
+            if (entry.isAvailableToAddChildren())
+                return;
+            if (entry.leftChild != null)
+                queue.add(entry.leftChild);
+            if (entry.rightChild != null)
+                queue.add(entry.rightChild);
+            if (!queue.isEmpty())
+                entry = queue.poll();
+        }
+
+        entry = root;
+        queue.add(entry);
+
+        while (!queue.isEmpty()) {
+
+            entry.availableToAddRightChildren = true;
+            entry.availableToAddLeftChildren = true;
+            entry.checkChildren();
+
+            if (entry.leftChild != null)
+                queue.add(entry.leftChild);
+            if (entry.rightChild != null)
+                queue.add(entry.rightChild);
+            if (!queue.isEmpty())
+                entry = queue.poll();
+        }
+
+
     }
 
     @Override
@@ -40,7 +164,12 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     }
 
     @Override
-    public void add(int index, String element) {
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
         throw new UnsupportedOperationException();
     }
 
@@ -50,62 +179,44 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     }
 
     @Override
-    protected void removeRange(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
     public List<String> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public String remove(int index) {
+    protected void removeRange(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
 
-    public CustomTree() {
-        root = new Entry<>("0");
-        treeList.add(0, root);
-    }
-
     @Override
-    public boolean add(String s) {
-        Entry<String> current;
-        Entry<String> newEntry = new Entry<String>(s);
-
-        for(Entry<String> entry: treeList) {
-            current = entry;
-            if (current.isAvailableToAddChildren()) {
-                if (current.leftChild == null) {
-                    current.leftChild = newEntry;
-                    current.availableToAddLeftChildren = false;
-                } else if (current.rightChild == null) {
-                    current.rightChild = newEntry;
-                    current.availableToAddRightChildren = false;
-                }
-                newEntry.parent = current;
-                treeList.add(newEntry);
-                return true;
-            }
-        }
-        return false;
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException();
     }
-
 
     static class Entry<T> implements Serializable {
         String elementName;
+        int lineNumber;
         boolean availableToAddLeftChildren, availableToAddRightChildren;
         Entry<T> parent, leftChild, rightChild;
 
-        public Entry (String name) {
-            elementName = name;
+        public Entry(String elementName) {
+            this.elementName = elementName;
             availableToAddLeftChildren = true;
             availableToAddRightChildren = true;
+
         }
-        public boolean isAvailableToAddChildren () {
-            return availableToAddLeftChildren || availableToAddRightChildren;
+
+        public void checkChildren() {
+            if (leftChild != null)
+                availableToAddLeftChildren = false;
+
+            if (rightChild != null)
+                availableToAddRightChildren = false;
+
+        }
+
+        boolean isAvailableToAddChildren() {
+            return (availableToAddLeftChildren || availableToAddRightChildren);
         }
     }
-
 }
