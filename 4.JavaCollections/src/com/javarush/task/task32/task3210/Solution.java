e65c22fad7bbf850aaaa0aa5ba126edef5978d5a
swd762 <ilyakazalin@yandex.ru>
2020-10-18 11:07:02 +0400

    completed task 3210

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -1,14 +1,55 @@
 package com.javarush.task.task32.task3210;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 /* 
 Используем RandomAccessFile
+В метод main приходят три параметра:
+1) fileName - путь к файлу;
+2) number - число, позиция в файле;
+3) text - текст.
+
+Считать текст с файла начиная с позиции number, длиной такой же как и длина переданного текста в третьем параметре.
+Если считанный текст такой же как и text, то записать в конец файла строку 'true', иначе записать 'false'.
+Используй RandomAccessFile и его методы seek(long pos), read(byte[] b, int off, int len), write(byte[] b).
+Используй один из конструкторов класса String для конвертации считанной строчки в текст.
+
+
+Требования:
+1. В методе main класса Solution необходимо использовать RandomAccessFile, который должен использовать файл, который приходит первым параметром.
+2. В методе main класса Solution программа должна устанавливать позицию в файле, которая передана во втором параметре.
+3. В методе main класса Solution программа должна считывать данные с файла при помощи метода read(byte[] b, int off, int len).
+4. Запись должна происходить в конец файла.
+5. Если считанный текст такой же как и text, то программа должна записать в конец переданного файла строку 'true'.
+6. Если считанный текст НЕ такой же как и text, то программа должна записать в конец переданного файла строку 'false'.
 */
 
 public class Solution {
     public static void main(String... args) {
+        try {
+            RandomAccessFile raf = new RandomAccessFile(args[0], "rw");
+            int len = args[2].length();
+            int pos = Integer.parseInt(args[1]);
+            raf.seek(pos);
+            byte[] b = new byte[len];
+            raf.read(b,0,len);
 
+            raf.seek(raf.length());
+            if(!args[2].equals(new String(b))){
+
+                raf.write("false".getBytes());
+            }
+            else{
+                raf.write("true".getBytes());
+            }
+        }
+        catch(FileNotFoundException e){
+
+        }
+        catch(IOException e){
+
+        }
     }
 }
