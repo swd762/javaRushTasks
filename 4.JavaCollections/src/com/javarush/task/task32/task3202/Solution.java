6b52f9e0eaf2bb30ad1a8d456187d679147ad855
swd762 <ilyakazalin@yandex.ru>
2020-10-20 13:01:34 +0400

    completed task 3203

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task32.task3202;
+
+import java.io.*;
+
+/* 
+Читаем из потока
+Реализуй логику метода getAllDataFromInputStream. Он должен вернуть StringWriter, содержащий все данные из переданного потока.
+Возвращаемый объект ни при каких условиях не должен быть null.
+Метод main не участвует в тестировании.
+
+
+Требования:
+1. Публичный статический метод getAllDataFromInputStream (InputStream) должен существовать.
+2. Метод getAllDataFromInputStream (InputStream) должен возвращать StringWriter.
+3. Метод getAllDataFromInputStream (InputStream) должен вернуть StringWriter, который содержит все данные из переданного потока.
+4. Возвращаемый объект ни при каких условиях не должен быть null.
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        StringWriter sw = new StringWriter();
+        String line = null;
+        if (is != null) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sw.write(line);
+            }
+        }
+        return sw;
+    }
+}
