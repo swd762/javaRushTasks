c9ca90898d517d2d21b92243303d6102ee419ce9
swd762 <ilya01041984@gmail.com>
2021-02-26 16:30:19 +0400

    solved several tasks

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -2,4 +2,8 @@ package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject {
 
+    public Stand(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
 }
