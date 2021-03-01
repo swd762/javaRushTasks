04c0de7b6cbaf4a9a9a7abdabf2f2a1eff6ead7c
swd762 <ilya01041984@gmail.com>
2021-03-01 15:34:35 +0400

    solved several tasks

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -2,13 +2,34 @@ package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject {
 
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 
     @Override
     public void move() {
+        double dx = speed * direction;
+        x += dx;
+    }
 
+    void moveLeft() {
+        direction = -1;
+    }
+
+    void moveRight() {
+        direction = 1;
     }
 
     @Override
