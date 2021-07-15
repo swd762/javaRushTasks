375ea3b10005479a241b2d22c8c1f08e7fe80519
swd762 <ilya01041984@gmail.com>
2021-07-15 18:44:27 +0400

    solved several tasks

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+public class MainModel implements Model {
+
+    private ModelData modelData;
+    private UserService userService;
+
+    public MainModel() {
+        modelData = new ModelData();
+        userService = new UserServiceImpl();
+    }
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+
+}
