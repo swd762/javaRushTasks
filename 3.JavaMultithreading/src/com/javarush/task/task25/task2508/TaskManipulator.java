cabee93b4a9c7b0aa0c186a2931f5742e3ee31bb
swd762 <ilya01041984@gmail.com>
2021-08-09 16:52:05 +0400

    solved several tasks

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -1,7 +1,29 @@
 package com.javarush.task.task25.task2508;
 
-public class TaskManipulator implements Runnable {
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+
+    private Thread currentThread;
     @Override
     public void run() {
+        while(!currentThread.isInterrupted()){
+            try {
+                System.out.println(currentThread.getName());
+                Thread.sleep(100);
+            } catch(InterruptedException e){
+                currentThread.interrupt();
+            }
+        }
     }
+
+    @Override
+    public void start(String threadName) {
+        currentThread = new Thread(this, threadName);
+        currentThread.start();
+    }
+
+    @Override
+    public void stop() {
+        currentThread.interrupt();
+    }
+
 }
