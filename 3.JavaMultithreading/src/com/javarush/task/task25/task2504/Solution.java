8098b544585c03654268fbe764fc5218d1041064
swd762 <ilya01041984@gmail.com>
2021-08-09 14:28:28 +0400

    solved several tasks

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -5,10 +5,31 @@ Switch для нитей
 */
 
 public class Solution {
+
     public static void processThreads(Thread... threads) {
         //implement this method - реализуйте этот метод
+        for (Thread thread : threads) {
+            switch (thread.getState()) {
+                case NEW:
+                    thread.start();
+                    break;
+                case BLOCKED:
+                case TIMED_WAITING:
+                case WAITING:
+                    thread.interrupt();
+                    break;
+                case RUNNABLE:
+                    thread.isInterrupted();
+                    break;
+
+                case TERMINATED:
+                    System.out.println(thread.getPriority());
+                    break;
+            }
+        }
     }
 
     public static void main(String[] args) {
     }
+
 }
