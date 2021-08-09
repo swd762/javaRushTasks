b189b0790e8bbf709c9a228ace34c9559a2edb3b
swd762 <ilya01041984@gmail.com>
2021-08-09 15:41:48 +0400

    solved several tasks

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2505/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2505/Solution.java
@@ -12,19 +12,40 @@ public class Solution {
     }
 
     public class MyThread extends Thread {
+
         private String secretKey;
 
         public MyThread(String secretKey) {
             this.secretKey = secretKey;
             setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
-            setDaemon(true);
+//            setDaemon(false);
         }
 
         @Override
         public void run() {
             throw new NullPointerException("it's an example");
+
         }
+
+        private class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+            public MyUncaughtExceptionHandler() {
+            }
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                try {
+                    Thread.sleep(500);
+                    System.out.println(String.format("%s, %s, %s", secretKey, t.getName(), e.getMessage()));
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+
     }
+
 
 }
 
