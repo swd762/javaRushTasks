8ba05887c3d8559d0edf3ca4649348cf7936773a
swd762 <ilya01041984@gmail.com>
2021-08-29 09:22:36 +0400

    solved several tasks

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -7,8 +7,23 @@ package com.javarush.task.task25.task2510;
 public class Solution extends Thread {
 
     public Solution() {
+        this.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                String message;
+                if (e instanceof Error) {
+                    message = "Нельзя дальше работать";
+                } else if (e instanceof Exception) {
+                    message = "Надо обработать";
+                } else {
+                    message = "Поживем - увидим";
+                }
+                System.out.println(message);
+            }
+        });
     }
 
     public static void main(String[] args) {
     }
+
 }
