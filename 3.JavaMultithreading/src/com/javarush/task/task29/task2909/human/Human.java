b9e351b2e79265769100e8335130897d6546b9b2
swd762 <ilya01041984@gmail.com>
2021-08-30 12:29:33 +0400

    solved several tasks

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -63,10 +63,6 @@ public class Human implements Alive {
     }
 
 
-
-
-
-
     public int getId() {
         return id;
     }
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Teacher.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Teacher.java
@@ -1,9 +1,9 @@
 package com.javarush.task.task29.task2909.human;
 
-public class Teacher extends Human {
+public class Teacher extends UniversityPerson {
 
     private int numberOfStudents;
-    private String university;
+
 
     public Teacher(String name, int age, int numberOfStudents) {
         super(name, age);
@@ -18,16 +18,9 @@ public class Teacher extends Human {
     public void teach() {
     }
 
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
-    }
 
     public void printData() {
-        System.out.println("Преподаватель: " + name);
+        System.out.println("Преподаватель: " + getUniversity());
     }
 
 }
