4cec7d468aa7cb6869baa1882f09f12551e2b447
swd762 <ilya01041984@gmail.com>
2022-01-09 14:16:45 +0400

    2707 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task27.task2707;
+
+/* 
+Определяем порядок захвата монитора
+*/
+
+public class Solution {
+
+    private static volatile boolean flag = false;
+    private static volatile boolean isInnerThreadBlocked = false;
+
+    public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
+        //do something here
+        synchronized (o1) {
+            Thread daemonThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Thread innerThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            solution.someMethodWithSynchronizedBlocks(o1, o2);
+                        }
+                    });
+                    innerThread.start();
+                    while (innerThread.getState() != Thread.State.BLOCKED);
+                        isInnerThreadBlocked = true;
+
+                    synchronized (o2) {
+                        flag = true;
+                    }
+
+                }
+            });
+            daemonThread.setDaemon(true);
+            daemonThread.start();
+            while (!isInnerThreadBlocked) {
+                Thread.sleep(1);
+            }
+            while (daemonThread.getState() != Thread.State.BLOCKED && daemonThread.isAlive()) {
+                Thread.sleep(1);
+            }
+        }
+
+
+
+        return flag;
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Solution solution = new Solution();
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+
+        System.out.println(isLockOrderNormal(solution, o1, o2));
+    }
+
+    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            synchronized (obj2) {
+                System.out.println(obj1 + " " + obj2);
+            }
+        }
+    }
+
+}
